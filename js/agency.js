f608d1bdbf276ce16a42bff9b0a73f126751a7db
Yohan Belval <Yohan Belval>
2016-04-11 18:19:52 -0400

    reste le contenu à arranger

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,28 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// Google maps configuration
+var canvas = $("#map-canvas")[0];
+var coordinates = new google.maps.LatLng(46.45130,-73.00160 );
+var options = {
+center: coordinates,
+zoom: 8,
+scrollwheel: false,
+mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+
+var map = new google.maps.Map(canvas, options);
+var marker = new google.maps.Marker({
+    position: coordinates,
+    map: map,
+    title: 'Le Baluchon Éco-villégiature',
+    url: "https://www.google.ca/maps/place/Le+Baluchon+%C3%89co-vill%C3%A9giature/@46.4513735,-73.0038276,17z/data=!4m2!3m1!1s0x0000000000000000:0x945f4633aee4825f"
+});
+
+google.maps.event.addListener(marker, 'click', function() {
+    window.location.href = this.url;
+});
+
+// Slippry slider configuration
+$("#slider").slippry({ pager: false, autoHover: false });
